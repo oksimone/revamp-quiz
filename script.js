465effb62d1958b13f94562c4e1cf49c8c54bdd0
ksimone <ksworthy@windstream.net>
2020-10-29 09:37:02 -0400

    changes to the countdown function and the event listener

--- script.js
+++ script.js
@@ -1,53 +1,32 @@
-var timeEl = document.getElementById("timer")
-
-// will add more questions later
-var questions = [
-    {q: "fill in the blank. Hydro _____", 
-    choices: ["Mug", "Cup", "Flask", "Flute"],
-    answer:"Flask"
-
-},
-{
-    question: "what is the color of a sunflower?",
-    choices: ["yellow","red", "violet", "blue"],
-    answer:"yellow"
-},
-{
-    question: "the newest iPhone is missing which accessory?",
-    choices: ["charger", "earphones", "the box", "the sticker"],
-    answer: "charger"
-},
-{
-    question: "when does daylight savings time end?",
-    choices: ["october 31st", "november 3rd","november 1st", "october 30th"],
-    answer: "november 1st"
-}
-];
-
-var timer = 30;
-
-function setTime(){
-    var timerInterval = setInterval (function (){
-        timer--;
-        timeEl.textContent = timer;
-
-        if(timer === 0){
-            clearInterval(timerInterval);
-            window.location.href = "https://twitter.com"
+document.addEventListener("DOMContentLoaded", () => {
+    const beginbtn = document.querySelector("#beginBtn");
+  
+    beginbtn.addEventListener("click", begin);
+    
+  });
+  
+  function begin(){
+      window.location.href = "questions.html"
+  }
+  
+    function countDown() {
+      const timeLeftDisplay = document.querySelector("#timer");
+      let secondsLeft = 45;
+      setInterval(function () {
+        if (secondsLeft <= 0) {
+          clearInterval(secondsLeft);
+          window.location.href = "highscores.html";
         }
-
-    }, 1000
-    )
-}
-setTime();
-
-
-
-
-
-
-document.getElementById("beginBtn").addEventListener("click", begin);
-
-function begin(){
-    window.location.href = "questions.html"
-}
+        timeLeftDisplay.innerHTML = secondsLeft;
+        secondsLeft -= 1;
+      }, 1000);
+    }
+  
+    let url = window.location.href;
+    
+    window.onload = function () {
+      if (url.indexOf("questions.html") !== -1) {
+        countDown();
+      }
+    };
+  
