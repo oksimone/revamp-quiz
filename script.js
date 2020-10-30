99ede02154fc189adf833b6f71ca4f6c71b56934
ksimone <ksworthy@windstream.net>
2020-10-30 16:39:59 -0400

    made some present questions so that I could see if the questions would populate to the page

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
+// not sure what current index is, must figure that out
+var currentIndex = 0;
+var allQuestions = 0;
+let secondsLeft = 45;
+
 document.addEventListener("DOMContentLoaded", () => {
-    const beginbtn = document.querySelector("#beginBtn");
-  
-    beginbtn.addEventListener("click", begin);
-    
-  });
-  
-  function begin(){
-      window.location.href = "questions.html"
-  }
-  
-    function countDown() {
-      const timeLeftDisplay = document.querySelector("#timer");
-      let secondsLeft = 45;
-      setInterval(function () {
-        if (secondsLeft <= 0) {
-          clearInterval(secondsLeft);
-          window.location.href = "highscores.html";
-        }
-        timeLeftDisplay.innerHTML = secondsLeft;
-        secondsLeft -= 1;
-      }, 1000);
+  const beginbtn = document.querySelector("#beginBtn");
+
+  beginbtn.addEventListener("click", begin);
+});
+
+function begin() {
+  window.location.href = "questions.html";
+ 
+}
+
+function countDown() {
+  const timeLeftDisplay = document.querySelector("#timer");
+  setInterval(function () {
+    if (secondsLeft <= 0) {
+      clearInterval(secondsLeft);
+      window.location.href = "highscore.html";
     }
-  
-    let url = window.location.href;
-    
-    window.onload = function () {
-      if (url.indexOf("questions.html") !== -1) {
-        countDown();
-      }
-    };
-  
+    timeLeftDisplay.innerHTML = secondsLeft;
+    secondsLeft -= 1;
+  }, 1000);
+}
+
+let url = window.location.href;
+
+window.onload = function () {
+  if (url.indexOf("questions.html") !== -1) {
+    countDown();
+    writeQuestion();
+  }
+};
+
+// questions
+var allQuestions = [
+  {
+    question: "fill in the blank, Hydro _____",
+    choices: ["cup", "mug", "flask", "flute"],
+    answer: "flask",
+  },
+  {
+    question: "the sky is what color?",
+    choices: ["blue", "pink", "white", "depends on the day"],
+    answer: "depends on the day",
+  },
+  {
+    question: "what color is coffee?",
+    choices: ["black", "yellow", "tan", "beige"],
+    answer: "black",
+  },
+  {
+    question:
+      "how many licks does it take to get to the center of a tootsie pop?",
+    choices: ["four", "twenty", "the world may never know", "ten"],
+    answer: "the world may never know",
+  },
+];
+
+function writeQuestion() {
+  var questions = document.querySelector("#description");
+  var currentQuestion = allQuestions[currentIndex];
+  const choiceContainer = document.querySelector("#questionContain");
+  questions.textContent = currentQuestion.question;
+  choiceContainer.innerHTML = "";
+  // const choices = document.querySelector("#choiceOne");
+
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var newAnswerBtn = document.createElement("button");
+    newAnswerBtn.setAttribute("value", currentQuestion.choices[i]);
+    newAnswerBtn.textContent = currentQuestion.choices[i];
+    // newAnswerBtn.setAttribute("style", "display: visible")
+    choiceContainer.appendChild(newAnswerBtn);
+    // onclick function for clicking the answer
+    newAnswerBtn.onclick = answerClick;
+    console.log(currentQuestion)
+  }
+}
+
+function answerClick() {
+  if (this.value !== allQuestions[currentIndex.answer]) {
+    secondsLeft = secondsLeft - 10;
+  }
+
+  currentIndex++;
+
+  if (currentIndex === allQuestions.length) {
+    console.log("end game");
+  } else {
+    writeQuestion();
+  }
+}
